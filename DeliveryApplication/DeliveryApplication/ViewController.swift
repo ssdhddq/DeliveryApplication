20c55f5e3a2fc8c1dccfe80116aadb955cf4fbe5
ssdhddq <ssdhddq@vk.com>
2025-02-05 19:05:54 +0700

    Create base project, delete storyboard

---
+++ DeliveryApplication/DeliveryApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeliveryApplication
+//
+//  Created by SSDHDDQ on 05.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+
+
+}
+
