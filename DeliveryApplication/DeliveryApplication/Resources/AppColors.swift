f815fa1bdaa05f55812dcd341595a78afba83139
ssdhddq <ssdhddq@vk.com>
2025-02-05 21:57:35 +0700

    add new base colors

---
+++ DeliveryApplication/DeliveryApplication/Resources/AppColors.swift
@@ -0,0 +1,15 @@
+//
+//  AppColors.swift
+//  DeliveryApplication
+//
+//  Created by SSDHDDQ on 05.02.2025.
+//
+
+import UIKit
+
+enum AppColors {
+    static let accentOrange = UIColor(hex: "#D35400FF")
+    static let gray = UIColor(hex: "#ECF0F1FF")
+    static let black = UIColor(hex: "#000000FF")
+    static let background = UIColor(hex: "#E5E5E5FF")
+}
