153808a722c581380c3e785fb1fd63563eab4c99
ssdhddq <ssdhddq@vk.com>
2025-02-05 20:46:29 +0700

    edit fonts(-)

---
+++ DeliveryApplication/DeliveryApplication/Resources/Extensions/UIFont+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  UIFont+Extension.swift
+//  DeliveryApplication
+//
+//  Created by SSDHDDQ on 05.02.2025.
+//
+
+import UIKit
+
+extension UIFont {
+    
+}
+
+private extension UIFont {
+    enum Constants {
+        enum Roboto {
+            
+        }
+    }
+}
