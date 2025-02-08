7159d7a25640b1952bde294e9971534f34fcfc52
ssdhddq <ssdhddq@vk.com>
2025-02-09 00:11:47 +0700

    Added onboarding screen

--- DeliveryApplication/DeliveryApplication/NavigationLayer/Coordinators/AppCoordinator.swift
+++ DeliveryApplication/DeliveryApplication/NavigationLayer/Coordinators/AppCoordinator.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class AppCoordinator: Coordinator{
     override func start() {
-        //showOnboardingFlow()
-        showMainFlow()
+        showOnboardingFlow()
+        //showMainFlow()
     }
     
     override func finish() {
